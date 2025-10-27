a688d9ff8d650234d431a07b7f5180e6942aefc1
Ali Jalili <mrali97jl@gmail.com>
2025-10-27 12:46:34 +0330

    `Added async init function and renderRecipes function stub`

--- 02-recipe-book/app.js
+++ 02-recipe-book/app.js
@@ -3,3 +3,28 @@ import { fetchRandomRecipe } from "./utils/api.js";
 // 2. DOM Selection: انتخاب عناصر DOM مورد نیاز
 const recipeListEl = document.getElementById('recipe-list');
 const statusMessageEl = document.querySelector('.container p'); // فرض می‌کنیم اولین <p> در container پیام وضعیت است
+
+
+async function init() {
+
+    statusMessageEl.textContent = 'در حال بارگذاری دستورالعمل‌ها... ⌛';
+
+    const recipeData = await fetchRandomRecipe();
+
+    if (recipeData) {
+        statusMessageEl.textContent = '';
+
+        renderRecipes(recipeData);
+
+    } else {
+        statusMessageEl.textContent = '❌ متأسفانه، در حال حاضر امکان دریافت دستورالعمل‌ها وجود ندارد.';
+    }
+
+
+
+}
+
+
+const renderRecipes = function () {
+
+}
